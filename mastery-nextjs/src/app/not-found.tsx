0ff52e7485aea3ce0cac85f02e9f155e30bff126
Artur <arturrodriguesmota@hotmail.com>
2024-05-16 09:55:26 -0300

    7: Add custom not found page and add it to other page

---
+++ mastery-nextjs/src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div>
+      <h1>Page Not Found</h1>
+      <p>This is a custom not found page</p>
+    </div>
+  );
+}
--- mastery-nextjs/src/app/posts/[postId]/page.tsx
+++ mastery-nextjs/src/app/posts/[postId]/page.tsx
@@ -1,5 +1,7 @@
-export default function PostId({ params }: { params: { postId: string }}){
+import { notFound } from "next/navigation"
 
+export default function PostId({ params }: { params: { postId: string }}){
+  if(parseInt(params.postId) > 10) return notFound()
   return (
     <h1>This is a individual page to a post {params.postId}</h1>
   )
