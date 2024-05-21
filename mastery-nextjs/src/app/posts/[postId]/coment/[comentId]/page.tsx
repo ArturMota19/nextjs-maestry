116e497a8e343500e6c6d56a36fb0b970376bb53
Artur <arturrodriguesmota@hotmail.com>
2024-05-21 09:49:55 -0300

    19: Create error boundary in specific route

--- mastery-nextjs/src/app/posts/[postId]/coment/[comentId]/page.tsx
+++ mastery-nextjs/src/app/posts/[postId]/coment/[comentId]/page.tsx
@@ -1,5 +1,15 @@
+import {notFound} from "next/navigation"
+
+function simulateError(count:number) {
+  return Math.floor(Math.random()*count);
+}
+
 export default function ComentDetail({ params }: { params: { postId: string, comentId: string }}
 ){
+  const random = simulateError(2);
+  if(random === 1) {
+    throw new Error("Failed to load data");
+  }
   return (
     <div>
       <h1>This is a individual page to a coment</h1>
