ab368ec26eab17eb0d1612da0a003f4f39b4a136
Artur <arturrodriguesmota@hotmail.com>
2024-05-23 09:42:17 -0300

    25: Add conditional routes

---
+++ mastery-nextjs/src/app/complex-dashboard/@login/default.tsx
@@ -0,0 +1,6 @@
+import Card from "@/components/card";
+
+export default function DefaultLogin() {
+  return <Card>Please Login to Continue</Card>
+
+}
--- mastery-nextjs/src/app/complex-dashboard/layout.tsx
+++ mastery-nextjs/src/app/complex-dashboard/layout.tsx
@@ -4,18 +4,21 @@ export default function RootLayout({
   children,
   users,
   revenue,
-  notifications
+  notifications,
+  login,
 }: {
   children: React.ReactNode;
   users: React.ReactNode;
   revenue: React.ReactNode;
   notifications: React.ReactNode;
+  login: React.ReactNode;
 }) {
   // parallel routes are important because they allow us to render multiple components at the same time
   // and each component can be responsible for its own data fetching and states
   // this is a good way to split the logic and make the code more maintainable
 
-  return (
+  const isLoggedIn = false; // hard code bc we don't have backend
+  return isLoggedIn ?(
     <>
       <div>{children}</div>
       <div style={{display: "flex"}}>
@@ -26,5 +29,7 @@ export default function RootLayout({
         <div style={{display: "flex", flex: "1"}}>{notifications}</div>
       </div>
     </>
-  )
+  ) : (
+    <div>{login}</div>
+  ) ;
 }
