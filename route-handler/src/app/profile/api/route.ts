304527017ede13489d912a69dbe7372ee175ca97
Artur <arturrodriguesmota@hotmail.com>
2024-05-27 09:14:23 -0300

    35: Headers in Route Handlers

--- route-handler/src/app/profile/api/route.ts
+++ route-handler/src/app/profile/api/route.ts
@@ -1,3 +1,18 @@
-export async function GET() {
-  return new Response("Profile Data")
+import { type NextRequest } from "next/server"
+import { headers } from "next/headers"
+export async function GET(request: NextRequest) {
+  // first way is using the nextRequest
+  const requestHeaders = new Headers(request.headers)
+  const authorization = requestHeaders.get("Authorization")
+
+  // second way is using the headers helper
+  const headersList = headers()
+  const headerAuthorization = headersList.get("Authorization")
+
+  console.log(authorization, headerAuthorization)
+  return new Response("<h1>Profile API -  HTML Headers</h1>", {
+    headers: {
+      "Content-Type": "text/html",
+    },
+  })
 }
