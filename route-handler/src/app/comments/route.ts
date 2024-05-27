67d76876eb47acd7213c596be871afacd0d20ba5
Artur <arturrodriguesmota@hotmail.com>
2024-05-27 08:47:05 -0300

    29: Handler a POST request

--- route-handler/src/app/comments/route.ts
+++ route-handler/src/app/comments/route.ts
@@ -3,3 +3,17 @@ import { comments } from "./data"
 export async function GET(){
   return Response.json(comments)
 }
+export async function POST(request: Request){
+  const comment = await request.json()
+  const singleComment = {
+    id: comments.length + 1,
+    text: comment.text
+  }
+  comments.push(singleComment)
+  return new Response(JSON.stringify(singleComment), {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    status: 201
+  })
+}
