aa7a720dc4b3d086be1699c224a83949f7b68618
Artur <arturrodriguesmota@hotmail.com>
2024-05-27 09:19:18 -0300

    36: Cookies in Route Handlers

--- route-handler/src/app/profile/api/route.ts
+++ route-handler/src/app/profile/api/route.ts
@@ -1,5 +1,5 @@
 import { type NextRequest } from "next/server"
-import { headers } from "next/headers"
+import { headers, cookies } from "next/headers"
 export async function GET(request: NextRequest) {
   // first way is using the nextRequest
   const requestHeaders = new Headers(request.headers)
@@ -9,10 +9,19 @@ export async function GET(request: NextRequest) {
   const headersList = headers()
   const headerAuthorization = headersList.get("Authorization")
 
+  // first way to get cookies
+  const theme = request.cookies.get("theme")
+
+  // second way to get cookies
+  cookies().set("resultsPerPage", "10")
+  const resultsPerPage = cookies().get("resultsPerPage") // we can algo use another functions, like delete, getAll, etc
+
   console.log(authorization, headerAuthorization)
+  console.log("Cookies: ", theme, resultsPerPage)
   return new Response("<h1>Profile API -  HTML Headers</h1>", {
     headers: {
       "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark"
     },
   })
 }
